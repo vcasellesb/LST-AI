697dc2fce7884b7d595d42bdf22353677150752f
Vicent Caselles <vcasellesb@uoc.edu>
2025-10-04 18:17:24 +0200

    fix error where dirname(__file__) is at /usr/local/bin when downloading weights and shit

--- LST_AI/utils.py
+++ LST_AI/utils.py
@@ -3,14 +3,16 @@ import os
 import zipfile
 from urllib import request
 
-def download_data(path):
+MODELS_PATH = os.path.dirname(__file__)
+
+def maybe_download_data():
     """
     Downloads required model weights, binaries and atlas files for usage.
     """
     url = "https://github.com/CompImg/LST-AI/releases/download/v1.0.0/lst_data.zip"
 
     target_path = "lst_data.zip"
-    extract_path = path  # This is the base directory.
+    extract_path = MODELS_PATH  # This is the base directory.
 
     atlas_path = os.path.join(extract_path, 'atlas')
     binary_path = os.path.join(extract_path, 'binaries')
